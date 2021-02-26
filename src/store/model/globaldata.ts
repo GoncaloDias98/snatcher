1798e009337d65781970f49a45bc88857f956405
Gonçalo Dias <32722225+GoncaloDias98@users.noreply.github.com>
2021-02-26 03:12:10 +0000

    Revert "."
    This reverts commit cc9d3461a03968de82881068713086c1281692af.

---
+++ src/store/model/globaldata.ts
@@ -0,0 +1,168 @@
+import {Store} from './store';
+
+export const Globaldata: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container:
+        'body > main > div.main-container > div.container.mb-5 > div.row.align-items-lg-start.z-above-pattern > ck-product-cta-box > div > div.d-flex.mt-4 > div.w-100 > ck-addto-cart-wrapper > form > div > div.w-100.pl-3 > button > span',
+      text: ['Adicionar ao carrinho'],
+    },
+    outOfStock: {
+      container:
+        'body > main > div.main-container > div.container.mb-5 > div.row.align-items-lg-start.z-above-pattern > ck-product-cta-box > div > div.d-flex.mt-4 > div.w-100 > button',
+      text: ['Notifique-me quando estiver em stock'],
+    },
+  },
+  links: [
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url:
+        'https://www.globaldata.pt/drive-externa-dvdr-asus-sdrw-08d2s-u-lite-8x-branco-usb-20-90-dq0436-ua221kz',
+    },
+    {
+      brand: 'asus',
+      model: 'ekwb',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-asus-geforce-rtx-3080-ek-10gd6x-90yv0f60-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-asus-geforce-rtx-3080-rog-strix-oc-10gd6x-90yv0fa1-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc white',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-asus-geforce-rtx-3080-rog-strix-oc-white-10gd6x-90yv0fa5-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'strix white',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-asus-geforce-rtx-3080-rog-strix-white-10gd6x-90yv0fa6-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-asus-geforce-rtx-3080-tuf-gaming-10gd6x-90yv0fb0-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-asus-geforce-rtx-3080-tuf-gaming-oc-10gd6x-90yv0fb1-m0nm00',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-aorus-master-10gb-gd6x-gvn3080aorusm',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-aorus-master-oc-10gb-gd6x-gvn3080am-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-aorus-xtreme-10gb-gd6x-gvn3080ax-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-eagle-10gb-gd6x-gv-n3080eagle-10gd',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-eagle-oc-10gb-gd6x-gvn3080eagleoc10gd',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-gaming-oc-10gb-gd6x-gvn3080gamingoc10g',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-gigabyte-geforce-rtx-3080-vision-oc-10gb-gd6x-gvn3080visoc-10',
+    },
+    {
+      brand: 'msi',
+      model: 'gaming x trio',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-msi-geforce-rtx-3080-gaming-x-trio-10g-4719072762544',
+    },
+    {
+      brand: 'msi',
+      model: 'suprim x',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-msi-geforce-rtx-3080-suprim-x-10g-4719072762537',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-msi-geforce-rtx-3080-ventus-3x-10g-oc-4719072762520',
+    },
+    {
+      brand: 'zotac',
+      model: 'amp extreme holo',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-zotac-geforce-rtx-3080-amp-extreme-holo-10gb-gd6x-zt-a30800b-10p',
+    },
+    {
+      brand: 'zotac',
+      model: 'amp holo',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-zotac-geforce-rtx-3080-amp-holo-10gb-gd6x-zt-a30800f-10p',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-zotac-geforce-rtx-3080-trinity-10gb-gd6x-zt-a30800d-10p',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity oc',
+      series: '3080',
+      url:
+        'https://www.globaldata.pt/grafica-zotac-geforce-rtx-3080-trinity-oc-10gb-gd6x-zt-a30800j-10p',
+    },
+    ],
+  name: 'globaldata',
+  waitUntil: 'domcontentloaded',
+};
